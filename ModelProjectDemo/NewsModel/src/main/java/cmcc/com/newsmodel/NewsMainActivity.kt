a02f92e4edd0276b5c3deb2962c4ba2107754c78
chenguandong <chenguandong@limijiaoyin.com>
2019-01-21 15:01:09 +0800

    kotlin 模块传参成功

--- ModelProjectDemo/NewsModel/src/main/java/cmcc/com/newsmodel/NewsMainActivity.kt
+++ ModelProjectDemo/NewsModel/src/main/java/cmcc/com/newsmodel/NewsMainActivity.kt
@@ -2,6 +2,7 @@ package cmcc.com.newsmodel
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import guandong.com.sharemodule.ShareTools
@@ -9,12 +10,13 @@ import kotlinx.android.synthetic.main.activity_news_main.*
 
 @Route(path = "/news/newsMain")
 class NewsMainActivity : AppCompatActivity() {
-
+    @Autowired
+    @JvmField var name: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ARouter.getInstance().inject(this)
         setContentView(R.layout.activity_news_main)
-        newsTextView.text = "新闻"
+        newsTextView.text = name
 
         shareButton.setOnClickListener {
                 ShareTools.showShareView(this)
